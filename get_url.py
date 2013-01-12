ae3bc3164e95c9230432815f945fd8bde971f60e
Shiwen Zhang <clydecheung@gmail.com>
2013-01-12 15:09:20 +0800

    'get_url.py' now take stdin as input if no input file is specified

--- get_url.py
+++ get_url.py
@@ -2,17 +2,16 @@
 
 import sys
 
-if len(sys.argv) != 2:
-    #sys.stderr.write('No input file.')
-    print >> sys.stderr, 'No input file.'
-    exit(1)
+def get_url(file):
+    content = file.read()
+    pointer = content.find('wikicontent')
+    pointer = content.find('href', pointer)
+    pointer = pointer + 6
+    str_end = content.find('"', pointer)
+    print content[pointer:str_end]
 
-file_name = sys.argv[1]
-file = open(file_name)
-content = file.read()
-
-pointer = content.find('wikicontent')
-pointer = content.find('href', pointer)
-pointer = pointer + 6
-str_end = content.find('"', pointer)
-print content[pointer:str_end]
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        get_url(sys.stdin)
+    else:
+        get_url(open(sys.argv[1]))
